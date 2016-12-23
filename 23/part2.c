5c60c0da5b5d6596cffc0c0f852bee44ea4f0446
HighTide1 <jeredt1@gmail.com>
2016-12-23 17:44:58 -0500

    Updated Day 23's Solutions to Part 1 & 2 in C. Optimized to use enums instead of sscanfs.

--- 23/part2.c
+++ 23/part2.c
@@ -5,29 +5,67 @@
 #define BUFFER_SIZE 1024
 #define NUM_REGS 4
 
+// Parameter Type. Can either be a REGISTER or IMMEDIATE.
+enum PARAM_TYPE{
+    REGISTER,
+    IMMEDIATE
+};
+
+// Parameter Structure. Acts as an intermmediate type for
+// the parameters of an instruction, being either a register
+// or an immediate value.
+typedef struct param_t{
+    enum PARAM_TYPE type;
+    union {
+        char reg;
+        int  val;
+    };
+} PARAM;
+
+// Instruction ID. Instructions can only be a set number of values.
+enum INSTR_ID{
+    INC,
+    DEC,
+    CPY,
+    JNZ,
+    TGL,
+    INV
+};
+
 // Instruction Structure. Basically acts as a holder for previous
 // instructions, such that the jump instruction will work.
 typedef struct instr_t{
-    char   buffer[BUFFER_SIZE];
-    int    num_args;
+    enum INSTR_ID   id;
+    PARAM           param1;
+    PARAM           param2;
     struct instr_t* prev;
     struct instr_t* next;
 } INSTR;
 INSTR* head = NULL;
 INSTR* tail = NULL;
 
+// Instruction Format Strings.
+const char* inc_f   = "inc %c";
+const char* dec_f   = "dec %c";
+const char* tgl_rf  = "tgl %c";
+const char* tgl_if  = "tgl %d";
+const char* cpy_rrf = "cpy %c %c";
+const char* cpy_irf = "cpy %d %c";
+const char* jnz_rrf = "jnz %c %c";
+const char* jnz_rif = "jnz %c %d";
+const char* jnz_irf = "jnz %d %c";
+const char* jnz_iif = "jnz %d %d";
+
 // Function Prototypes
 void add_instr(char* buffer);
 void change_instr(INSTR* curr_instr, int num_away);
-int  is_valid(INSTR* curr_instr);
 
 int main(int argc, char** argv){
     FILE*  input_txt;
     char   buffer[BUFFER_SIZE];
     int    regs[NUM_REGS] = {0, 0, 0, 0};
-    char   reg, tmp_reg;
-    int    val, tmp_val;
     INSTR* curr_instr;
+    int    val, jmp_val;
 
     // Read in the input file specified by argv
     if(argc==1){
@@ -45,75 +83,74 @@ int main(int argc, char** argv){
     // Build a linked list of instructions from the file
     fgets(buffer, BUFFER_SIZE, input_txt);
     while(feof(input_txt)==0){
+        //printf("%s", buffer);
         add_instr(buffer);
         fgets(buffer, BUFFER_SIZE, input_txt);
     }
 
     curr_instr = head;
     while(curr_instr!=NULL){
-        memcpy(buffer, curr_instr->buffer, BUFFER_SIZE);
-        printf("%s", buffer);
-        if(is_valid(curr_instr)==1){
-            if(sscanf(buffer, "jnz %d %d", &tmp_val, &val)==2){
-                if(tmp_val!=0){
-                    while(val!=0 && curr_instr!=NULL && curr_instr!=head){
-                        if(val<0){
-                            curr_instr = curr_instr->prev;
-                            val++;
-                        }else{
-                            curr_instr = curr_instr->next;
-                            val--;
-                        }
-                    }
-                }else{
-                    curr_instr = curr_instr->next;
-                }
-            }else if(sscanf(buffer, "jnz %d %c", &tmp_val, &reg)==2){
-                if(tmp_val!=0){
-                    val = regs[reg-'a'];
-                    while(val!=0 && curr_instr!=NULL && curr_instr!=head){
-                        if(val<0){
-                            curr_instr = curr_instr->prev;
-                            val++;
-                        }else{
-                            curr_instr = curr_instr->next;
-                            val--;
-                        }
-                    }
-                }else{
-                    curr_instr = curr_instr->next;
-                }
-            }else if(sscanf(buffer, "jnz %c %d", &reg, &val)==2){
-                if(regs[reg-'a']!=0){
-                    while(val!=0 && curr_instr!=NULL && curr_instr!=head){
-                        if(val<0){
-                            curr_instr = curr_instr->prev;
-                            val++;
-                        }else{
-                            curr_instr = curr_instr->next;
-                            val--;
-                        }
-                    }
-                }else{
-                    curr_instr = curr_instr->next;
-                }
-            }else if(sscanf(buffer, "tgl %c", &reg)==1){
-                change_instr(curr_instr, regs[reg-'a']);
+        switch(curr_instr->id){
+            case INC:
+                regs[(curr_instr->param1).reg-'a']++;
                 curr_instr = curr_instr->next;
-            }else{
-                if(sscanf(buffer, "cpy %d %c", &val, &reg)==2){
-                    regs[reg-'a'] = val;
-                }else if(sscanf(buffer, "cpy %c %c", &reg, &tmp_reg)==2){
-                    regs[tmp_reg-'a'] = regs[reg-'a'];
-                }else if(sscanf(buffer, "inc %c", &reg)==1){
-                    regs[reg-'a'] += 1;
-                }else if(sscanf(buffer, "dec %c", &reg)==1){
-                    regs[reg-'a'] -= 1;
+                break;
+            case DEC:
+                regs[(curr_instr->param1).reg-'a']--;
+                curr_instr = curr_instr->next;
+                break;
+            case TGL:
+                if((curr_instr->param1).type==REGISTER){
+                    change_instr(curr_instr, regs[(curr_instr->param1).reg-'a']);
+                }else{
+                    change_instr(curr_instr, (curr_instr->param1).val);
                 }
                 curr_instr = curr_instr->next;
-            }
-            printf("(%d, %d, %d, %d)\n", regs[0], regs[1], regs[2], regs[3]);
-        } 
+                break;
+            case CPY:
+                if((curr_instr->param2).type!=IMMEDIATE){
+                    if((curr_instr->param1).type==REGISTER){
+                        val = regs[(curr_instr->param1).reg-'a'];
+                    }else{
+                        val = (curr_instr->param1).val;
+                    }
+                    regs[(curr_instr->param2).reg-'a'] = val;
+                }
+                curr_instr = curr_instr->next;
+                break;
+            case JNZ:
+                // For JNZ Instructions, we need first to get out comparison
+                // value, and our jump amount.
+                if((curr_instr->param1).type==REGISTER){
+                    val = regs[(curr_instr->param1).reg-'a'];
+                }else{
+                    val = (curr_instr->param1).val;
+                }
+                if((curr_instr->param2).type==REGISTER){
+                    jmp_val = regs[(curr_instr->param2).reg-'a'];
+                }else{
+                    jmp_val = (curr_instr->param2).val;
+                }
+
+                if(val!=0){
+                    while(jmp_val!=0 && curr_instr!=NULL){
+                        if(jmp_val<0){
+                            curr_instr = curr_instr->prev;
+                            jmp_val++;
+                        }else{
+                            curr_instr = curr_instr->next;
+                            jmp_val--;
+                        }
+                    }
+                }else{
+                    curr_instr = curr_instr->next;
+                }
+                break;
+            default:
+                curr_instr = curr_instr->next;
+                break;
+        }
+        //printf("Regs: %d, %d, %d, %d\n", regs[0], regs[1], regs[2], regs[3]);
     }   
  
     fclose(input_txt);  // Close the file for cleanup;
@@ -125,13 +162,54 @@ int main(int argc, char** argv){
 // Creates a new instruction structure and adds it to the end of the list.
 void add_instr(char* buffer){
     INSTR* new_instr = (INSTR*)malloc(sizeof(INSTR));
-    memcpy(new_instr->buffer, buffer, BUFFER_SIZE);
-    
-    if(buffer[0]=='i' || buffer[0]=='d' || buffer[0]=='t'){
-        new_instr->num_args = 1;
+    PARAM  p1, p2;
+
+    // For adding instructions, we have two primary types to consider:
+    // 1 Parameter and 2 Parameter Instructions.
+    // There are 4 1-Param Type Instructions to consider (INC, DEC, and TGL)
+    // There are 6 2-Param Type Instructions to consider (CPY and JNZ) 
+    if(sscanf(buffer, inc_f, &p1.reg)==1){
+        new_instr->id = INC;
+        p1.type = REGISTER;
+    }else if(sscanf(buffer, dec_f, &p1.reg)==1){
+        new_instr->id = DEC;
+        p1.type = REGISTER;
+    }else if(sscanf(buffer, tgl_if, &p1.val)==1){
+        new_instr->id = TGL;
+        p1.type = IMMEDIATE;
+    }else if(sscanf(buffer, tgl_rf, &p1.reg)==1){
+        new_instr->id = TGL;
+        p1.type = REGISTER;
+    }else if(sscanf(buffer, cpy_irf, &p1.val, &p2.reg)==2){
+        new_instr->id = CPY;
+        p1.type = IMMEDIATE;
+        p2.type = REGISTER;
+    }else if(sscanf(buffer, cpy_rrf, &p1.val, &p2.reg)==2){
+        new_instr->id = CPY;
+        p1.type = REGISTER;
+        p2.type = REGISTER;
+    }else if(sscanf(buffer, jnz_iif, &p1.val, &p2.val)==2){
+        new_instr->id = JNZ;
+        p1.type = IMMEDIATE;
+        p2.type = IMMEDIATE;
+    }else if(sscanf(buffer, jnz_irf, &p1.val, &p2.reg)==2){
+        new_instr->id = JNZ;
+        p1.type = IMMEDIATE;
+        p2.type = REGISTER;
+    }else if(sscanf(buffer, jnz_rif, &p1.reg, &p2.val)==2){
+        new_instr->id = JNZ;
+        p1.type = REGISTER;
+        p2.type = IMMEDIATE;
+    }else if(sscanf(buffer, jnz_rrf, &p1.reg, &p2.reg)==2){
+        new_instr->id = JNZ;
+        p1.type = REGISTER;
+        p2.type = REGISTER;
     }else{
-        new_instr->num_args = 2;
+        new_instr->id = INV;
     }
+
+    new_instr->param1 = p1;
+    new_instr->param2 = p2;
 
     if(head==NULL){
         new_instr->prev = NULL;
@@ -165,43 +243,25 @@ void change_instr(INSTR* curr_instr, int num_away){
     // For toggle instructions, we only consider those that would modify existing
     // instructions, and would not modify themselves.
     if(temp!=NULL && temp!=curr_instr){
-        if(temp->num_args==1){
-            if(temp->buffer[0]=='i'){
-                temp->buffer[0] = 'd';
-                temp->buffer[1] = 'e';
-                temp->buffer[2] = 'c';
-            }else{
-                temp->buffer[0] = 'i';
-                temp->buffer[1] = 'n';
-                temp->buffer[2] = 'c';
-            }
-        }else{
-            if(temp->buffer[0]=='j'){
-                temp->buffer[0] = 'c';
-                temp->buffer[1] = 'p';
-                temp->buffer[2] = 'y';
-            }else{
-                temp->buffer[0] = 'j';
-                temp->buffer[1] = 'n';
-                temp->buffer[2] = 'z';
-            }
+        // For 1-Param Instructions, there are 2 cases. Either an INC changes to a DEC, or anything
+        // changes to INC.
+        switch(temp->id){
+            case INC:
+                temp->id = DEC;
+                break;
+            case DEC:
+            case TGL:
+                temp->id = INC;
+                break;
+            case CPY:
+                temp->id = JNZ;
+                break;
+            case JNZ:
+                temp->id = CPY;
+                break;
+            default:
+                break;
         }
     }
 }
 
-// Determines if the given buffer contains a valid instruction (i.e. one not made invalid
-// by the tgl instruction.).
-int is_valid(INSTR* curr_instr){
-    int v1, v2;
-    
-    if(curr_instr->num_args==1){
-        return 1;
-    }else{
-        if(sscanf(curr_instr->buffer, "cpy %d %d", &v1, &v2)==2){
-            printf("Invalid Instr.\n");
-            return 0;
-        }else{
-            return 1;
-        }
-    }    
-}
